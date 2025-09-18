eedb6b82b5c633989340fd49d8f02825c89a4516
marakeshamq <vladislav.ivanchenko@e-u.edu.ua>
2025-09-18 11:46:57 +0300

    Update hw2.asm

--- homework/hw2.asm
+++ homework/hw2.asm
@@ -1,15 +1,1 @@
-TARGET = hw1
-ASM_SRC = $(TARGET).asm
-OBJ_FILE = $(TARGET).o
-EXEC_FILE = program
 
-build:
-	nasm -f elf32 $(ASM_SRC) -o $(OBJ_FILE)
-
-link:
-	ld -m elf_i386 $(OBJ_FILE) -o $(EXEC_FILE)
-
-run:
-	./$(EXEC_FILE)
-
-all: build link run
